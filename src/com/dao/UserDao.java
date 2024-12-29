2904688eaeec6185ea4e48faf22ac2693642597f
Varad Mule <varadmule17@gmail.com>
2024-12-29 21:40:07 +0530

    Dao update added

--- src/com/dao/UserDao.java
+++ src/com/dao/UserDao.java
@@ -40,7 +40,42 @@ public class UserDao {
 
 	}
 
-	
+	// update
+	public void updateData(Users u) {
+		Connection c = DBConnection.getConnection();
+		if (c != null) {
+			try {
+				String query = "update userdetails set firstname=?, lastname=?, email = ?, phoneno = ? where uid = ?";
+				PreparedStatement ps = c.prepareStatement(query);
+
+				ps.setString(1, u.getFirstName());
+				ps.setString(2, u.getLastName());
+				ps.setString(3, u.getEmail());
+				ps.setInt(4, u.getPhoneNo());
+				ps.setInt(5, u.getUid());
+
+				int result = ps.executeUpdate();
+
+				if (result > 0) {
+					System.out.println("Data Updated...");
+				} else {
+					System.out.println("Data Not Updated...");
+				}
+				ps.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					if (c != null) {
+						c.close();
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+	}
 	
 	
 	
