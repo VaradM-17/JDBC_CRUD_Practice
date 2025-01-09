374c5ab73aa51ba32bc1d43db332ec816f8d8962
Varad Mule <varadmule17@gmail.com>
2025-01-09 22:16:23 +0530

    dao displayall added

--- src/com/dao/UserDao.java
+++ src/com/dao/UserDao.java
@@ -2,6 +2,7 @@ package com.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import com.connection.DBConnection;
 import com.entity.Users;
@@ -127,4 +128,45 @@ public class UserDao {
 		}
 	}
 
+	// Dislay all data
+	public void displayAll() {
+
+		Connection c = null;
+		PreparedStatement ps = null;
+
+		try {
+			c = DBConnection.getConnection();
+			if (c != null) {
+				String query = "select * from userdetails";
+				ps = c.prepareStatement(query);
+
+				ResultSet rs = ps.executeQuery();
+
+				while (rs.next()) 
+				{
+					int uid = rs.getInt("uid");
+					String firstname = rs.getString("firstname");
+					String lastname = rs.getString("lastname");
+					String email = rs.getString("email");
+					int phoneno = rs.getInt("phoneno");
+
+					System.out.println("User Id : " + uid + ", First Name : " + firstname + ", Last Name: " + lastname
+							+ ", Email Id : " + email + ", Phone Number : " + phoneno);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (ps != null) {
+					ps.close();
+				}
+				if (c != null) {
+					c.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
